91ba3809b653d9fef7b0234186b83ebef28b8935
Anji <zam58mrp@i.softbank.jp>
2026-09-01 15:50:07 +0900

    docs: checkpoint use cases before split

---
+++ src/main/java/com/example/wealth_platform/WealthPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.wealth_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WealthPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WealthPlatformApplication.class, args);
+	}
+
+}
